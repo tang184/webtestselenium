abbf6caf810ca6dbfd134ada4f562dbc0263adc1
Juncheng Tang <tang184@purdue.edu>
2017-05-16 18:43:27 -0400

    finally works!

--- mysite/sum/tests.py
+++ mysite/sum/tests.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from seleniumrequests import Chrome
+from selenium.webdriver.common.by import By
 
 
 # Create your tests here
@@ -17,32 +19,56 @@ class FooTest(TestCase):
 		pass
 
 
+
 	def test_two(self):
-		browser = webdriver.Chrome()
-
-		browser.get('http://www.yahoo.com')
-		assert 'Yahoo' in browser.title
-
-		elem = browser.find_element_by_name('p')  # Find the search box
-		elem.send_keys('seleniumhq' + Keys.RETURN)
-
-		browser.quit()
-
-	def test_three(self):
+		
 		browser = webdriver.Chrome()
 		browser.get('http://127.0.0.1:8000/sum')
-		try:
-			element = WebDriverWait(browser, 10). until(
-				EC.presence_of_element_located((By.Name, "sum"))
-			)
-			x = browser.find_element_by_name('x')  # Find the search box
-			x.send_keys(1)
-			y = browser.find_element_by_name('y')
-			y.send_keys(2)
-			y.send_keys(Keys.RETURN)
-			browser.implicitly_wait(5)
-			print (browser.find_element_by_name('sum').get_attribute('value'))
+		x = browser.find_element_by_name('x')  # Find the search box
+		x.send_keys(5)
+		y = browser.find_element_by_name('y')
+		y.send_keys(2)
+		browser.find_element_by_name('subbut').click()
+		#browser.implicitly_wait(10)
+		#browser.get('http://127.0.0.1:8000/sum')
+		#browser.implicitly_wait(10)
+		WebDriverWait(browser, 5).until(
+		    EC.text_to_be_present_in_element(
+		        (By.ID, 'sum'),
+		        '7'
+		    )
+		)
 
-		finally:
-			assert (browser.find_element_by_name('sum').get_attribute('value') == 3)
-			# browser.quit()
+
+
+	def test_three(self):
+		
+		browser = webdriver.Chrome()
+		browser.get('http://127.0.0.1:8000/sum')
+		x = browser.find_element_by_name('x')  # Find the search box
+		x.send_keys(1)
+		y = browser.find_element_by_name('y')
+		y.send_keys(2)
+		browser.find_element_by_name('subbut').click()
+		#browser.implicitly_wait(10)
+		#browser.get('http://127.0.0.1:8000/sum')
+		#browser.implicitly_wait(10)
+		WebDriverWait(browser, 5).until(
+		    EC.text_to_be_present_in_element(
+		        (By.ID, 'sum'),
+		        '3'
+		    )
+		)
+		
+		"""
+		#assert (bintsum.get_attribute('value') == None)
+		# browser.quit()
+		browser = Chrome()
+		response = browser.request('POST', 'http://127.0.0.1:8000/sum/', data={"x": "1", "y": "2"})
+		#browser.get('http://127.0.0.1:8000/sum')
+		browser.implicitly_wait(5)
+		#print(response)
+		#intsum = browser.find_element_by_name('sum')
+		#print ("INt sum value is" + intsum.get_attribute('value'))
+		pass
+		"""
